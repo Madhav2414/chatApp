8f51543aed7d9eb9618a6a17f647ab33e36bccbf
dhruvYadv <mypc2716@gmail.com>
2023-12-29 08:57:35 +0530

    404 added

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,27 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom';
 import About from './view/About';
 import HomePage from './view/pages/HomePage';
-import MessageInput from './components/MessageBar/messageInput';
 import Sidebar from './components/SideBar/Sidebar';
 import Chat from './components/UserChat/SenderChat';
 import ChatSearch from './components/UserChatHeader/ChatSearch';
 import Navbar from './components/NavBar/Navbar';
+import Page404 from './view/pages/Page404';
 import LandingPage from './view/pages/LandingPage/LandingPage';
+
 function App() {
 
   return (
     <>
       <Routes>
       <Route path='/landingPage' element={<LandingPage />} />
-        <Route path='/Navbar' element={<Navbar />} />
+        <Route path='/Navbar' element={<Navbar/>} />
         <Route path='/ChatSearch' element={<ChatSearch />} />
         <Route path='/UserChat' element={<Chat />} />
-        <Route path='/message' element={<MessageInput />} />
         <Route path="/" element={<HomePage />} />
         <Route path="/sidebar" element={<Sidebar />} />
         <Route path="/about" element={<About />} />
+        <Route path="*" element={< Page404 />} />
+
       </Routes>
 
     </>
